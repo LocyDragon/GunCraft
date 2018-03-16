804a12674c04362efedf207c95cd81468fac7b31
LocyDragon <2424441676@qq.com>
2018-03-16 20:32:52 +0800

    create git

---
+++ src/main/java/com.guncraft.guncraft/GunCraft.java
@@ -0,0 +1,12 @@
+package com.guncraft.guncraft;
+
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GunCraft extends JavaPlugin implements Listener {
+	@Override
+	public void onEnable() {
+
+	}
+}
